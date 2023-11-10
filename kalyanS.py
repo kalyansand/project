dbc751f3d9f0d4b1fc27116a4ae18e51267d7bc4
Sandeep Reddy Patllola <psandeepreddy22@gmail.com>
2023-11-09 23:06:27 -0600

    Project document requirements

--- kalyanS.py
+++ kalyanS.py
@@ -1,3 +1,28 @@
-with open('/Users/sandeepreddy/Desktop/kalyan.txt', 'r') as file:
+with open('/Users/sandeepreddy/Desktop/kalyan/project/kalyan.txt', 'r') as file:
     for line in file:
-        print(line, end='')  # end='' to avoid double new lines when printing
+        print(line, end='') 
+
+
+
+
+
+
+
+# def msd_trace_file(file_path="/Users/sandeepreddy/Desktop/kalyan/project/kalyan.txt"):
+#     with open(file_path, 'r') as file:
+#         lines = file.readline()
+#         print(lines)
+#         for f in lines:
+#             cores = f.split('')
+#             print(cores)
+
+
+
+
+
+# user_specified_file = "/Users/sandeepreddy/Desktop/kalyan/project/kalyan.txt"
+# if user_specified_file:
+#    msd_trace_file(user_specified_file)
+
+
+
