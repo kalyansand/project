2da3b3557a8d859e22bf5e6f1d98f4f85954d821
Sandeep Reddy Patllola <psandeepreddy22@gmail.com>
2023-11-09 23:38:29 -0600

    saved

--- kalyanS.py
+++ kalyanS.py
@@ -1,42 +1,34 @@
-with open('/Users/sandeepreddy/Desktop/kalyan/project/kalyan.txt', 'r') as file:
-    for line in file:
-        print(line, end='') 
-
-
-
-
-
-
-
-# def msd_trace_file(file_path="/Users/sandeepreddy/Desktop/kalyan/project/kalyan.txt"):
-#     with open(file_path, 'r') as file:
-#         lines = file.readline()
-#         print(lines)
-#         for f in lines:
-#             cores = f.split('')
-#             print(cores)
-
-
-
-
-
-# user_specified_file = "/Users/sandeepreddy/Desktop/kalyan/project/kalyan.txt"
-# if user_specified_file:
-#    msd_trace_file(user_specified_file)
-
-
-
 import sys
-#input_file = open("trace.txt", 'r')
-if len(sys.argv) != 2:
-    print("Usage: python split.py <input_file.txt>")
-    sys.exit(1)
-input_file = sys.argv[1]
-try:
-    with open(input_file, 'r') as file:
-        content = file.read()
-        print("Contents of the file:")
-        print(content)
-except FileNotFoundError:
-    # print(str("Error: File  "+  input_file + "not found."))
 
+# Define a function to read and print lines from a file
+def print_file_lines(file_name="/Users/sandeepreddy/Desktop/kalyan/project/kalyan.txt"):
+    try:
+        # Open the file in read mode
+        with open(file_name, 'r') as file:
+            # Read and print each line
+            for line in file:
+                print(line, end='')  # 'end='' to avoid double spacing due to newline characters
+    except FileNotFoundError:
+        print(f"Error: File '{file_name}' not found.")
+
+
+
+if __name__ == "__main__":
+    # will use when we give multiple files 
+    if len(sys.argv) > 1:
+        print("Usage: python3 xxx.py <file_name>")
+
+
+    # Get the file name from the command line argument
+    # file_name = sys.argv[1]
+
+    # # Call the function to print file lines
+    # print_file_lines(file_name)
+    
+    
+    if len(sys.argv) == 1:
+        print_file_lines()
+    else:
+        file_name = sys.argv[1]
+        print_file_lines(file_name)
+        
